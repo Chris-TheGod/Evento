e7146880a2f978e922bd488a8ac41c98ddaeeaa6
Kristjan Lääne <xonproject@gmail.com>
2024-10-03 17:06:18 +0300

    animate event cards

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,7 @@
 import clsx from 'clsx';
 import { ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
+import { EventoEvent } from './types';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -8,4 +9,20 @@ export function cn(...inputs: ClassValue[]) {
 
 export function capitalize(string: string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+export async function getEvents(city: string) {
+  const response = await fetch(
+    `https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`
+  );
+  const events: EventoEvent[] = await response.json();
+  return events;
+}
+
+export async function getEvent(slug: string) {
+  const response = await fetch(
+    `https://bytegrad.com/course-assets/projects/evento/api/events/${slug}`
+  );
+  const event = await response.json();
+  return event;
 }
